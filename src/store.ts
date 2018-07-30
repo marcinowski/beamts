391bbb2379f4219a0c559c5496beaeba14a6aabe
marcinowski <marcinowski007@gmail.com>
2018-07-30 23:07:31 +1000

    Using store

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,28 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    lines: [
+      {
+        id: 0,
+        x1: 300,
+        x2: 200,
+        y1: 300,
+        y2: 300,
+      },
+    ],
+    points: [
+      {
+        id: 1,
+        cx: 300,
+        cy: 300,
+      },
+      {
+        id: 2,
+        cx: 200,
+        cy: 300,
+      },
+    ],
   },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  mutations: {},
+  actions: {},
 });
